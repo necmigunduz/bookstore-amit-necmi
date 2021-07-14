1bab4393f5ff50924578aafc579a57bc0abfb61d
necmigunduz <necmigunduz@gmail.com>
2021-07-14 11:41:22 +0300

    Add handleRemoveBook

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Book = ({ bookId, title, category }) => (
+const Book = ({ bookId, title, category }, handleRemoveBook) => (
   <>
     <td>{bookId}</td>
     <td>{title}</td>
     <td>{category}</td>
+    <button type="submit" onClick={handleRemoveBook(bookId)}>Remove Book</button>
   </>
 );
 
@@ -13,6 +14,7 @@ Book.propTypes = {
   bookId: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   category: PropTypes.string.isRequired,
+  handleRemoveBook: PropTypes.func,
 };
 
 export default Book;
--- src/container/BooksList.js
+++ src/container/BooksList.js
@@ -4,16 +4,21 @@ import PropTypes from 'prop-types';
 import Book from '../components/Book';
 import { removeBook } from '../actions';
 
-const BooksList = ({ bookId, title, category }) => (
-  <>
-    <tr>
-      <th>BookId</th>
-      <th>Title</th>
-      <th>Category</th>
-      <Book bookId={bookId} title={title} category={category} />
-    </tr>
-  </>
-);
+const BooksList = ({ bookId, title, category }) => {
+  const handleRemoveBook = (bookId) => {
+    removeBook(bookId);
+  };
+  return (
+    <>
+      <tr>
+        <th>BookId</th>
+        <th>Title</th>
+        <th>Category</th>
+        { books.map((e) => <Book book={e} key={e.bookId} handleRemoveBook={handleRemoveBook} />)}
+      </tr>
+    </>
+  );
+};
 
 BooksList.propTypes = {
   bookId: PropTypes.number,
