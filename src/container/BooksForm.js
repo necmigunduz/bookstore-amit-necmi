6b8dcda15b0c0d7e51e39cd173621e34394ea51b
KumarAmitt <amitt2015@outlook.com>
2021-07-16 22:53:52 +0530

    Add input validation and category append issue

--- src/container/BooksForm.js
+++ src/container/BooksForm.js
@@ -7,7 +7,7 @@ const BooksForm = (props) => {
   const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
 
   const [title, setTitle] = useState('');
-  const [category, setCategory] = useState('');
+  const [category, setCategory] = useState('Action');
 
   const handleChange = (e) => {
     if (e.target.tagName === 'INPUT') {
@@ -21,6 +21,18 @@ const BooksForm = (props) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     const bookId = Math.random().toString();
+
+    const titleText = document.getElementsByName('BookTitle')[0];
+    if (/^$|\s+/.test(title)) {
+      titleText.placeholder = 'Book name can\'t be blank';
+      titleText.style.border = '1px solid red';
+      return;
+    }
+    if (title) {
+      titleText.placeholder = 'Book Title';
+      titleText.style.border = 'none';
+    }
+
     const book = {
       bookId,
       title,
@@ -36,7 +48,7 @@ const BooksForm = (props) => {
     <div className="form-container">
       <h2>Add New Book</h2>
       <form>
-        <input type="text" onChange={(e) => handleChange(e)} value={title} placeholder="Book Title" />
+        <input type="text" name="BookTitle" onChange={(e) => handleChange(e)} value={title} placeholder="Book Title" />
         <select onChange={(e) => handleChange(e)} value={category}>
           {
               categories.map((e) => <option key={Math.random()} value={e.id}>{e}</option>)
